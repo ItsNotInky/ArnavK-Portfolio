3b2dff8b91f52903fe53896a0ab7c47c02d71257
SpacianSPIFF <srikarduddu15@gmail.com>
2021-10-05 16:32:09 +0530

    Added onsubmit php handling, and css fixing

---
+++ src/pages/contact/contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $subject = "Contact form entry on website!"
+    $mailTo = "hello.arnavk@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.html?mailsend");
+}
