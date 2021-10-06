b9c8babf1bcca5fa4825e7919038372c116adc36
SpacianSPIFF <srikarduddu15@gmail.com>
2021-10-06 11:54:11 +0530

    Fixed contact

--- src/pages/contact/contactform.php
+++ src/pages/contact/contactform.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit'])) {
     $subject = "Contact form entry on website!"
     $mailTo = "hello.arnavk@gmail.com";
     $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $txt = "You have received an e-mail from ".$name." on your website.\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
     header("Location: contact.html?mailsend");
